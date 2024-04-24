0d00b37e68b9640fbea61d8ac90a985bcf0b9a9d
Tan Shan Chien <tanshanchien@gmail.com>
2024-04-24 16:38:02 +0800

    42 campus

--- srcs/data/init_data.c
+++ srcs/data/init_data.c
@@ -6,17 +6,18 @@
 /*   By: shatan <shatan@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 00:23:21 by stan              #+#    #+#             */
-/*   Updated: 2024/04/23 16:30:28 by shatan           ###   ########.fr       */
+/*   Updated: 2024/04/24 16:37:23 by shatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	error(const char *error_type, char *details, int ret)
+static inline int	handle_error()
 {
-	ft_printf("Error. %s: %s\n", error_type, details);
-	free(details);
-	return (ret);
+	if (errno == 0)
+		return (0);
+	ft_printf("Error.");
+	return (-1);
 }
 
 static int	init_list_a(t_list *a, const char **tokens, int len)
@@ -24,19 +25,20 @@ static int	init_list_a(t_list *a, const char **tokens, int len)
 	t_stringstream	*ss;
 	int				val;
 
-	ss = ss_create_with_str(ft_tokens_join(tokens, len, " "));
+	ss = ss_create_use_provided(ft_tokens_join(tokens, len, " "));
 	while (ss_read_int(ss, &val))
 	{
-		lst_append_val(a, val);
 		if (lst_find_val(a, val) != -1)
-			return (error("Repeating value", ft_itoa(val), -1));
+		{
+			errno = -1;
+			break ;
+		}
+		lst_append_val(a, val);
 	}
-	if (errno == ERANGE)
-		return (error("Integer out of range", ft_itoa(val), -1));
-	if (errno == EINVAL)
-		return (error("Not an Integer", ss_get_prev_str(ss), -1));
+	if (errno == EINVAL && ss_eof(ss_read_line(ss, NULL, " \n\t")))
+		errno = 0;
 	ss_destroy(ss);
-	return (0);
+	return (handle_error());
 }
 
 t_data	*init_data(int argc, const char **argv)
